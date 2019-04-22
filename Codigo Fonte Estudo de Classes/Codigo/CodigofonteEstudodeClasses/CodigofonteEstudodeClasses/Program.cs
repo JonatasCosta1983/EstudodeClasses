71e792a9a2ae144fd6975e5c4ef246ac78e3dc71
Jonatas Costa <costtagroup@gmail.com>
2019-04-22 18:50:07 -0300

    Adicionei um comentário ao form1

--- Codigo Fonte Estudo de Classes/Codigo/CodigofonteEstudodeClasses/CodigofonteEstudodeClasses/Program.cs
+++ Codigo Fonte Estudo de Classes/Codigo/CodigofonteEstudodeClasses/CodigofonteEstudodeClasses/Program.cs
@@ -16,7 +16,10 @@ namespace CodigofonteEstudodeClasses.inicio
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+
+
+            Application.Run(new Form1());// Aqui o sistema da início
         }
     }
 }
