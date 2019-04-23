2542bbe55fbfcb7c32535dfb80a8bbca1e9804bd
Jonatas Costa <costtagroup@gmail.com>
2019-04-22 21:20:25 -0300

    Implementei o Program, adicionando nele o frmSplash

--- Codigo Fonte Estudo de Classes/Codigo/CodigofonteEstudodeClasses/CodigofonteEstudodeClasses/Program.cs
+++ Codigo Fonte Estudo de Classes/Codigo/CodigofonteEstudodeClasses/CodigofonteEstudodeClasses/Program.cs
@@ -17,6 +17,10 @@ namespace CodigofonteEstudodeClasses.inicio
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            //Aqui o sistema está chamando o o splash.
+            frmSplash splash = new frmSplash();
+            splash.ShowDialog();
+
 
 
             Application.Run(new Form1());// Aqui o sistema da início
